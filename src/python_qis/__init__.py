3e6d8fb236bd81fce3bc6a3e7f269a6de84b75b2
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-09-25 23:25:40 +0200

    Bump version to 1.2.2 and update physics_constants type annotation for clarity

--- src/python_qis/__init__.py
+++ src/python_qis/__init__.py
@@ -2,8 +2,16 @@ import python_qis.math.general as general_math
 import python_qis.math.qis as qis_math
 import python_qis.cryptography_tools as cryptography_tools
 import python_qis.physics_tools as physics_tools
+from typing import Literal
 
-physics_constants: dict[str, complex | float] = {
+physics_constants: dict[
+    Literal[
+        "H_JOULE_SEC",
+        "HBAR_JOULE_SEC",
+        "BOLTZMANN_CONSTANT",
+        "SPEED_OF_LIGHT", 
+        "ELEMENTARY_CHARGE"
+    ], complex | float] = {
     "H_JOULE_SEC": physics_tools.H_JOULE_SEC,
     "HBAR_JOULE_SEC": physics_tools.HBAR_JOULE_SEC,
     "BOLTZMANN_CONSTANT": physics_tools.BOLTZMANN_CONSTANT,
